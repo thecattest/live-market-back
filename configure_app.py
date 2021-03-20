fc7c721b719fcd43e8b2a698a89a97e011478f0d
DSTU Hackathon Spring 2021 <hackathon@451407-ca74341.tmweb.ru>
2021-03-21 01:50:25 +0300

    cors

--- configure_app.py
+++ configure_app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_file, \
     request, redirect, make_response, session, abort, jsonify, \
     Response, Blueprint
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_cors import CORS
 
 from db_init import *
 from api import api_blueprint
@@ -14,7 +15,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.unauthorized_handler(callback=(lambda: redirect('/login')))
 app.config['SECRET_KEY'] = 'somenewsupersecretkeyforlivemarketappyouwillneverguess'
-
+CORS(app)
 
 @login_manager.user_loader
 def load_user(user_id):
